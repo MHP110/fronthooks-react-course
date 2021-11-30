707668d7bc9ad8039570a36ec59dbfa7c613bd4e
Mohammad Hasan Pouhasani <76045848+MHP110@users.noreply.github.com>
2021-11-30 21:21:04 +0330

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
+import ProductList from './Components/Products/ProductListClass';
 import NavBar from './Components/NavBar/NavBar'
 import TimerClass from './Components/Timer/timerClass';
 import ClickCounter from './Components/HocExample/clickCounter';
@@ -16,7 +17,9 @@ import { render } from '@testing-library/react';
 class App extends Component {
   state = {
     products: [
-      {title: "laptop", price: "99T", id: 1},
+      {name: "Laptop", price: "100T", quantity: "2", id: 1},
+      {name: "Pc", price: "90T", quantity: "2", id: 2},
+      {name: "Airpods", price: "80T", quantity: "2", id: 3},
     ],
   }
 
@@ -29,11 +32,11 @@ class App extends Component {
   }
   
   incrementHandler = (id) => {
-      // index
-      const index = this.state.products.findIndex((item) => item.id === id);
+    // index
+    const index = this.state.products.findIndex((item) => item.id === id);
     
-      //clone the selected index and update the quantity
-      const product = {...this.state.products[index]};
+    //clone the selected index and update the quantity
+    const product = {...this.state.products[index]};
     product.quantity++;
   
     //update products
@@ -46,7 +49,7 @@ class App extends Component {
       const index = this.state.products.findIndex((item) => item.id === id);
       const product = {...this.state.products[index]};
       if (product.quantity === 1) {
-            const filteredProducts = this.state.products.filter((p) => p.id !== id);
+        const filteredProducts = this.state.products.filter((p) => p.id !== id);
         this.setState({products: filteredProducts})
     } else {
           const products = [...this.state.products]
@@ -64,13 +67,13 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="">
         <CounterProvider>
           <p>welcome to counter</p>
           <CounterOne />
         </CounterProvider>
-          {/* <NavBar totalItems= {this.state.products.filter((p) => p.quantity > 0).length}/>
-          <Product 
+        {/* <NavBar totalItems= {this.state.products.filter((p) => p.quantity > 0).length}/>
+        <ProductList 
           products= {this.state.products} 
           onChange= {this.changeHandler}
           onIncrement= {this.incrementHandler}
